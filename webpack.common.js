7d28b942c521e9096fc9151fe301f751e747bb3e
Frank Lan <franklan118@gmail.com>
2018-05-31 23:48:31 +1000

    change loader to array

--- webpack.common.js
+++ webpack.common.js
@@ -16,9 +16,9 @@ module.exports = {
       {
         test: /\.(js|jsx)/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: [
+          "babel-loader",
+        ],
       },
     ],
   },
