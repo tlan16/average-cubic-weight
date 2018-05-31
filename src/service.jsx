1aca648bb57822a8f67b586cfd69d561e13f916f
Frank Lan <franklan118@gmail.com>
2018-06-01 00:08:44 +1000

    fix more coding style, this time is just my preference

--- src/service.jsx
+++ src/service.jsx
@@ -1,7 +1,6 @@
 const BASE_URL = 'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com'
 const API_ENDPOINT_START = '/api/products/1'
 
-
 async function fetchData(category) {
   return fetchRecursive(BASE_URL + API_ENDPOINT_START, category, [])
 }
@@ -10,17 +9,17 @@ async function fetchRecursive(api_endpoint, category, objects_accumulator) {
   const responseData = await fetchAction(api_endpoint)
   const objects_of_category = responseData.objects
     .filter(object =>
-      object.category == category
+      object.category === category
     )
   const next = responseData.next
-  if (!next)
-    return [...objects_accumulator, ...objects_of_category]
-  else
-    return fetchRecursive(BASE_URL + next, category, [...objects_accumulator, ...objects_of_category])
 
+  if (next)
+    return fetchRecursive(BASE_URL + next, category, [...objects_accumulator, ...objects_of_category])
+  else
+    return [...objects_accumulator, ...objects_of_category]
 }
 
-const fetchAction = (api_endpoint) => {
+const fetchAction = api_endpoint => {
   return fetch(api_endpoint)
     .then(response => response.json())
 }
