37378d70363c639a0078f5afbf9364ebbd82c6fd
Frank Lan <franklan118@gmail.com>
2018-05-31 23:48:54 +1000

    use happy pack for dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,34 @@
 const path = require('path')
 const merge = require('webpack-merge')
+const HappyPack = require('happypack')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/,
+        exclude: /node_modules/,
+        use: [
+          'happypack/loader',
+          "babel-loader",
+        ],
+      },
+    ],
+  },
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     hot: true,
   },
+  plugins: [
+    new HappyPack({
+      loaders: [
+        {
+          loader: 'babel-loader',
+        },
+      ],
+    }),
+  ],
 })
