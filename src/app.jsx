0bc0883097cf35cc5dd2883d2d8492928e30540d
Frank Lan <franklan118@gmail.com>
2018-06-01 00:15:31 +1000

    fix coding style

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react"
 import fetchData from "./service"
-import calculation from "./calculation"
+import {calculation} from "./calculation"
 
 class App extends Component {
   state = {
