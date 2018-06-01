ab0b9bf608a6b6b8294ffcb1e2d038af97a6d90e
Frank Lan <franklan118@gmail.com>
2018-06-01 10:57:15 +1000

    resort method order

--- src/services/category.js
+++ src/services/category.js
@@ -1,6 +1,10 @@
 const BASE_URL = process.env.API_URL
 export const API_ENDPOINT_START = '/api/products/1'
 
+const fetchAction = api_endpoint =>
+  fetch(api_endpoint)
+    .then(response => response.json())
+
 async function fetchRecursive(api_endpoint, category_name, objects_accumulator) {
   const responseData = await fetchAction(api_endpoint)
   const objects_of_category = responseData.objects
@@ -18,7 +22,3 @@ async function fetchRecursive(api_endpoint, category_name, objects_accumulator) 
 export default async function fetchData(category_name) {
   return fetchRecursive(BASE_URL + API_ENDPOINT_START, category_name, [])
 }
-
-const fetchAction = api_endpoint =>
-  fetch(api_endpoint)
-    .then(response => response.json())
